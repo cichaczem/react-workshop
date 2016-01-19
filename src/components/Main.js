69eace1385e489782adbdde1e572748479082b83
Dawid Cichuta <cichaczem@gmail.com>
2016-01-19 20:42:38 +0100

    first component

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,21 @@ import React from 'react';
 class Main extends React.Component {
   render() {
     return (
-      <h2>React Worshop</h2>
+      <div>
+        <header>
+          <a href="/">
+            <img src="../img/lunarts-logo-collapsed.png" className="logo"/>
+          </a>
+        </header>
+        <section className="main" aria-role="main">
+          <h1> School of Witchcraft & Wizardry </h1>
+          <img src="../img/wizard.png" className="wizard"/>
+          <div className="buttons-group">
+            <a href="#" className="button secondary">Enroll</a>
+            <a href="#" className="button secondary">See Students</a>
+          </div>
+        </section>
+      </div>
     )
   }
 };

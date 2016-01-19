c7828a32e0e97ca452b42d36a99171535e5d6ff4
Ania Slimak <anna.slimak@lunarlogic.io>
2016-01-19 12:50:05 +0100

    Added missing component

--- src/application.js
+++ src/application.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from "./components/App"
+import Main from "./components/Main"
 
 var app = document.getElementById('app');
 
-ReactDOM.render(<App />, app);
+ReactDOM.render(<Main />, app);
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <h2>React Worshop</h2>
+    )
+  }
+};
+
+export default Main;

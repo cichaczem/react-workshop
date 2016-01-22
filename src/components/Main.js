8e954ac7858adaa2ac25cbbe865160e1256b903c
Dawid Cichuta <cichaczem@gmail.com>
2016-01-22 11:16:42 +0100

    component as a function

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './shared/Header';
+import HeaderFunction from './shared/HeaderFunction';
 import Menu from './shared/Menu';
 
 class Main extends React.Component {
   render() {
     return (
       <div>
-        <Header />
+        <HeaderFunction />
         <section className="main" aria-role="main">
           <h1> School of Witchcraft & Wizardry </h1>
           <img src="../img/wizard.png" className="wizard"/>
---
+++ src/components/shared/HeaderFunction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderFunction = (props) =>
+  (
+    <header>
+      <a href="/">
+        <img src="../img/lunarts-logo-collapsed.png" className="logo"/>
+      </a>
+      <p>NEW HEADER</p>
+    </header>
+  )
+
+export default HeaderFunction;

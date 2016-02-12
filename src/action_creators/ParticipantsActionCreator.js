b98559cfc5b83fb0f013b3cf8d0a8de5710818e8
Ania Slimak <anna.slimak@lunarlogic.io>
2016-02-12 16:49:16 +0100

    Moved api call to action creator

--- src/action_creators/ParticipantsActionCreator.js
+++ src/action_creators/ParticipantsActionCreator.js
@@ -1,10 +1,13 @@
 import ActionTypes from '../constants/ActionTypes';
 import AppDispatcher from '../AppDispatcher';
+import API from '../lib/API';
 
 const PaticipantsActionCreator = {
   requestStudents() {
+    const students = new API().getStudents();
     AppDispatcher.dispatch({
-      actionType: ActionTypes.REQUEST_STUDENTS
+      actionType: ActionTypes.REQUEST_STUDENTS,
+      students: students
     })
   }
 }
